6eb88cd4d8ea71bdb8b7e75a80c97ec779402080
Oleg <brezitskyyoleg@gmail.com>
2020-06-08 01:01:58 +0200

    Updates

---
+++ precache-manifest.5d7f29c827a8f586e9653a71bd3569d5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f01f116161b77a6831c0491ac71f10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bfab5f0855c484301fb",
+    "url": "/static/css/main.99882f35.chunk.css"
+  },
+  {
+    "revision": "fc56c7738fc2c0d19cfd",
+    "url": "/static/js/2.c5ae3304.chunk.js"
+  },
+  {
+    "revision": "6bfab5f0855c484301fb",
+    "url": "/static/js/main.744d71c1.chunk.js"
+  },
+  {
+    "revision": "dde34973d82dcf9e25e8",
+    "url": "/static/js/runtime-main.304e5e31.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "eae9c18cee82a8a1a52e654911f8fe83",
+    "url": "/static/media/PRODUCT SANS REGULAR.eae9c18c.TTF"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/static/media/talksCardBack.de3da806.svg"
+  }
+]);
